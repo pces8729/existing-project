7cc9ee4581d3e42c2621fe0bf762ac59fbeef473
chrisyang <chyeh930282@gmail.com>
2023-10-20 18:33:55 +0800

    updated

---
+++ script/script.js
@@ -0,0 +1,8 @@
+import "animate.css";
+
+import { createPopper } from "@popperjs/core";
+const popcorn = document.querySelector("#popcorn");
+const tooltip = document.querySelector("#tooltip");
+createPopper(popcorn, tooltip, {
+  placement: "top",
+});
